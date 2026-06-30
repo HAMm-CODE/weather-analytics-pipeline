3ac6ded14aecdcd764a119122926614870cce14f
Hamm-code <sentsdrimma123@gmail.com>
2026-07-01 00:07:51 +0300

    Fix import statement formatting and enhance code readability in extract.py

--- src/extract.py
+++ src/extract.py
@@ -4,5 +4,16 @@ extract.py
 Etracts weather data from the Open-Meteo API.
 """
 
-Import logging
+import logging
 import requests
+
+from src.config import (
+    API_URL,
+    LATITUDE,
+    LONGITUDE,
+    LOCATION_NAME,
+    HOURLY_VARIABLES,
+    FORECAST_DAYS,
+    TIMEZONE,
+    REQUEST_TIMEOUT
+)
